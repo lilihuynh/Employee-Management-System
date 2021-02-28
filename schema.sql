7d824c5bc1498d3ce7c3c0420979efb79c3eabe9
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-27 20:32:44 -0800

    create role table

--- schema.sql
+++ schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id),
+  PRIMARY KEY (id)
+);
